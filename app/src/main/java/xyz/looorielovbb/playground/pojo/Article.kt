a6053a0d8cc378831fca05b4f2ad9702e63d5888
陆磊 <lulei@yidingyun.com>
2023-04-14 02:04:19 +0800

    升级版本库

--- app/src/main/java/xyz/looorielovbb/playground/pojo/Article.kt
+++ app/src/main/java/xyz/looorielovbb/playground/pojo/Article.kt
@@ -1,49 +1,48 @@
 package xyz.looorielovbb.playground.pojo
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "articles")
 data class Article(
     @PrimaryKey
-    @SerializedName("id") @ColumnInfo(name = "id") val id: Int? = 0,
-    @SerializedName("adminAdd") val adminAdd: Boolean? =false,
-    @SerializedName("apkLink") val apkLink: String?= "",
-    @SerializedName("audit") val audit: Int? =0,
-    @SerializedName("author") val author: String?= "",
-    @SerializedName("canEdit") val canEdit: Boolean?=false,
-    @SerializedName("chapterId") val chapterId: Int?= 0,
-    @SerializedName("chapterName") val chapterName: String?= "",
-    @SerializedName("collect") val collect: Boolean?=false,
-    @SerializedName("courseId") val courseId: Int?= 0,
-    @SerializedName("desc") val desc: String?= "",
-    @SerializedName("descMd") val descMd: String?= "",
-    @SerializedName("envelopePic") val envelopePic: String?= "",
-    @SerializedName("fresh") val fresh: Boolean?=false,
-    @SerializedName("host") val host: String?= "",
-    @SerializedName("isAdminAdd") val isAdminAdd: Boolean?=false,
-    @SerializedName("link") val link: String?= "",
-    @SerializedName("niceDate") val niceDate: String?= "",
-    @SerializedName("niceShareDate") val niceShareDate: String?= "",
-    @SerializedName("origin") val origin: String?= "",
-    @SerializedName("prefix") val prefix: String?= "",
-    @SerializedName("projectLink") val projectLink: String?= "",
-    @SerializedName("publishTime") val publishTime: Int?= 0,
-    @SerializedName("realSuperChapterId")  val realSuperChapterId: Int?= 0,
-    @SerializedName("route") val route: Boolean?=false,
-    @SerializedName("selfVisible") val selfVisible: Int?= 0,
-    @SerializedName("shareDate") val shareDate: Int?= 0,
-    @SerializedName("shareUser") val shareUser: String?= "",
-    @SerializedName("superChapterId") val superChapterId: Int?= 0,
-    @SerializedName("superChapterName") val superChapterName: String?= "",
-    @SerializedName("tags") @Ignore val tags: ArrayList<String> = arrayListOf(),
-    @SerializedName("title") val title: String?= "",
-    @SerializedName("type") val type: Int?= 0,
-    @SerializedName("userId") val userId: Int?= 0,
-    @SerializedName("visible") val visible: Int?= 0,
-    @SerializedName("zan") val zan: Int?= 0
+    val id: Int,
+    val adminAdd: Boolean,
+    val apkLink: String,
+    val audit: Int,
+    val author: String,
+    val canEdit: Boolean,
+    val chapterId: Int,
+    val chapterName: String,
+    val collect: Boolean,
+    val courseId: Int,
+    val desc: String,
+    val descMd: String,
+    val envelopePic: String,
+    val fresh: Boolean,
+    val host: String,
+    val link: String,
+    val niceDate: String,
+    val niceShareDate: String,
+    val origin: String,
+    val prefix: String,
+    val projectLink: String,
+    val publishTime: Int,
+    val realSuperChapterId: Int,
+    val route: Boolean,
+    val selfVisible: Int,
+    val shareDate: Int,
+    val shareUser: String,
+    val superChapterId: Int,
+    val superChapterName: String,
+    val title: String,
+    val type: Int,
+    val userId: Int,
+    val visible: Int,
+    val zan: Int,
 
-)
+){
+
+    @Ignore var tags: List<String> = arrayListOf()
+}
