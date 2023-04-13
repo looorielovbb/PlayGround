12a943f73c97a1b7d0117f4df6fb3c2f3cc40e24
陆磊 <lulei@yidingyun.com>
2023-04-14 02:28:20 +0800

    参数修改

--- app/src/main/java/xyz/looorielovbb/playground/data/local/ArticleDao.kt
+++ app/src/main/java/xyz/looorielovbb/playground/data/local/ArticleDao.kt
@@ -4,17 +4,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import xyz.looorielovbb.playground.pojo.Article
 
 @Dao
 interface ArticleDao {
 
     @Query("SELECT * FROM articles")
-    suspend fun getAllArticles():List<Article>?
+    fun getAllArticles():Flow<List<Article>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertArticle(article: Article)
 
     @Query("SELECT * FROM articles WHERE id = :id")
-    suspend fun getArticleById(id: String): Article?
+    fun getArticleById(id: String): Flow<Article>
+
+
 }
